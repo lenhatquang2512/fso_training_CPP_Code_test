071d8d1b9fa96545bf0c862b5d648f3c415e039d
quangle <lenhatquang2512>
2024-01-26 23:33:06 +0700

    mod singleton, add mutex to guard

--- InhePolySingletonTest.cpp
+++ InhePolySingletonTest.cpp
@@ -1,11 +1,11 @@
 /**
- * @file polymorphism_test.cpp
- * @author Quang Le (quangle@umich.edu)
+ * @file InhePolySingletonTest.cpp
+ * @author Quang N Le
  * @brief Full test code to illustrate Inheritance and Polymorphism in C++
  *         also combining namespace, template and many keywords
- *          Using singleton Design Pattern
+ *          Using Eager/Lazy Singleton Design Pattern
  * @version 0.1
- * @date 2024-01-22
+ * @date 2024-01-26
  * 
  * @copyright Copyright (c) 2024
  * 
@@ -13,6 +13,7 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <mutex>
 
 #define PRINT_CMD(x) (std::cout << x << std::endl)
 
@@ -26,21 +27,25 @@ namespace PolySample{
 
 template <typename T>
 class Base  {
+private:
+    static Base* instance;
+    static std::mutex mLocker;
 protected:
     T a;
-    static Base* instance;
     // explicit Base(void) :
-    //     Base(static_cast<T>(200)){ std::cout << "Base constructor no args called" << std::endl;};
+    // Base(static_cast<T>(200)){ std::cout << "Base constructor no args called" << std::endl;};
     explicit Base() = default;
     explicit Base(T a_):
         a{a_}{ std::cout << "Base constructor with args called" << std::endl;};
 public:
-    Base(const Base &source) = delete; //delete copy constructor
+    // Base(const Base &source) = delete; //delete copy constructor
 
-    static Base* getInstance(T a_){
-        if(initstate == nullptr){
+    static Base* getInstance(T a_ = static_cast<T>(200)){
+        mLocker.lock();
+        if(instance == nullptr){
             instance = new Base<T>(a_);
         }
+        mLocker.unlock();
         return instance;
     }
     virtual ~Base() {std::cout << "Base destructor called" << std::endl;};
@@ -52,25 +57,32 @@ public:
 template<typename T>
 Base<T>* Base<T>::instance = nullptr;
 
+template<typename T>
+std::mutex Base<T>::mLocker;
+
 template <typename T>
 class Derived final : public Base<T>{
-protected:
-    T b;
+private:
     static Derived* instance;
+    static std::mutex mLocker;
     explicit Derived() = default;
     // explicit Derived(void): // explicit mark constructors to not implicitly convert types
     //     Derived{static_cast<T>(25),static_cast<T>(12)}{std::cout << "Derived constructor no args called" << std::endl;};
-    explicit Derived(T a_, T b_ = 1):
+    explicit Derived(T a_, T b_ = static_cast<T>(1)):
         Base<T>{a_}, b{b_}{
             std::cout << "Derived constructor with args called" << std::endl;
         };
+protected:
+    T b;
 public:
     
-    Derived(const Derived &source) = delete; //delete copy constructor
+    // Derived(const Derived &source) = delete; //delete copy constructor
     static Derived* getInstance(T a_, T b_){
+        mLocker.lock();
         if(instance == nullptr){
             instance = new Derived<T>(a_,b_);
         }
+        mLocker.unlock();
         return instance;
     }
     virtual ~Derived() {std::cout << "Derived destructor called" << std::endl;}
@@ -83,6 +95,9 @@ public:
 template<typename T>
 Derived<T>* Derived<T>::instance = nullptr;
 
+template<typename T>
+std::mutex Derived<T>::mLocker;
+
 template <typename T>
 void use(Base<T>* base) {
     base->display();
@@ -93,21 +108,21 @@ int main(int argc, char const *argv[])
 {
     //Base class pointer
     PRINT_CMD("-------------USING BASE CLASS POINTER:---------------------");
-    PolySample::Base<int> *p =  PolySample::Derived<int>::getInstance(20,24);
-    int result = p->getData();
-    std::cout << "The result is " << result << std::endl;
-    p->display();
-    delete p; 
+    // PolySample::Base<int> *p =  PolySample::Derived<int>::getInstance(20,24);
+    // int result = p->getData();
+    // std::cout << "The result is " << result << std::endl;
+    // p->display();
+    // delete p; 
     
     // // Base class reference
-    // PRINT_CMD("-------------USING BASE CLASS REFERENCE--------------------:");
+    PRINT_CMD("-------------USING BASE CLASS REFERENCE--------------------:");
     // PolySample::Derived<int> *deriv = PolySample::Derived<int>::getInstance(15,8);
     // auto deriv = PolySample::Derived<int>::getInstance(15,8);
     // PolySample::Base<int> &ref = *deriv;
     // ref.display();
 
     //Using with vector (ONLY 1 INSTANCE)
-    // PRINT_CMD("---------------USING WITH VECTOR OF RAW POINTERS:--------------");
+    PRINT_CMD("---------------USING WITH VECTOR OF RAW POINTERS:--------------");
     // std::vector<PolySample::Derived<int>*> deriveds { PolySample::Derived<int>::getInstance(12,34)}; //Only one instance
     // for (const auto item : deriveds) {
     //     use(item);
@@ -118,21 +133,19 @@ int main(int argc, char const *argv[])
     // }
 
     // //Using with smart pointer
-    // PRINT_CMD("---------------USING WITH SMART POINTER:-------------------------");
-    // std::shared_ptr<PolySample::Base<int>> pSmart = std::make_shared<PolySample::Derived<int>>(
-    //     *deriv);
+    PRINT_CMD("---------------USING WITH SMART POINTER:-------------------------");
+    // std::unique_ptr<PolySample::Base<int>> pSmart = 
+    // std::make_unique<PolySample::Derived<int>>(*deriv);
     // pSmart->display(); 
 
-    // std::vector<std::shared_ptr<PolySample::Base<int>>> vecSmarts;
-    // vecSmarts.push_back(std::make_shared<PolySample::Derived<int>>(deriv));
-    // vecSmarts.push_back(std::make_shared<PolySample::Derived<int>>(deriv));
-    // vecSmarts.push_back(std::make_shared<PolySample::Derived<int>>(deriv));
+    std::vector<std::unique_ptr<PolySample::Base<int>>> vecSmarts;
+    vecSmarts.push_back(std::make_unique<PolySample::Derived<int>>(
+        *(PolySample::Derived<int>::getInstance(15,8)))); // comment out delete copy constructor
 
-    // for(const auto &v: vecSmarts){
-    //     // v->display();
-    //     v->getData();
-    // }
-
+    for(const auto &v: vecSmarts){
+        v->display();
+        v->getData();
+    }
 
     // Test explicit
     // PolySample::Base<int> obj;
---
+++ gen_code.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /home/quang_le/quang_prog/test/test_code_cpp_tut/fso_training_git/
+
+clear
+
+#compile
+g++  -pthread -std=c++14 -Wall -O0 -g InhePolySingletonTest.cpp -o InhePolySingletonTest
+
+#not necessary , make this file executable
+chmod +x InhePolySingletonTest
+
+#run
+./InhePolySingletonTest
+
