76fa4cfd83ef518cfecf2cb2c92a8a5274ad7c6e
NgDKhanh <khanh1piecesky@gmail.com>
2024-01-31 22:36:30 +0700

    Complete observer with raw pointer

--- Observer_guru_Khanh.cpp
+++ Observer_guru_Khanh.cpp
@@ -28,61 +28,6 @@ enum Status
     UNSAVED
 };
 
-template<typename T>
-class EventManager
-{
-    std::list<Base<T>*> listBase;
-public:
-    void notify(std::string message, Status status)
-    {
-        for (auto i : listBase)
-        {
-            i->update(message, status);
-        }
-    }
-    void subcribe(Base<T>* base)
-    {
-        listBase.push_back(base);
-    }
-    void unsubcribe(Base<T>* base)
-    {
-        listBase.remove(base);
-    }
-};
-
-template<typename T>
-class EventCreator
-{
-    EventManager<T> event_manager;
-    std::string message;
-    Status status;
-public:
-    EventCreator()
-    {
-        message = "Hello from event creator";
-        status = Status::UNSAVED;
-        event_manager->notify(message, status);
-    }
-    void changeMessage()
-    {
-        message = "Change";
-        status = Status::CHANGE;
-        event_manager->notify(message, status);
-    }
-    void saveMessage()
-    {
-        message = "Save";
-        status = Status::SAVED;
-        event_manager->notify(message, status);
-    }
-    void unsaveMessage()
-    {
-        message = "Unsave";
-        status = Status::UNSAVED;
-        event_manager->notify(message, status);
-    }
-};
-
 template <typename T>
 class Base  {
 protected:
@@ -97,7 +42,7 @@ public:
     virtual inline T getData(void) const { std::cout << "Get base data " << this->a << std::endl; return this->a;};
     virtual inline void setData(T a_des) {a = a_des;};
     virtual inline void display(void) const = 0; // Pure virtual function
-    virtual inline void update(std::string message, Status status) const = 0;
+    virtual inline void update(std::string message, Status status, T number) const = 0;
     // virtual inline void display(void) const { std::cout << this->a << " "  << std::endl;}; // Pure virtual function
 };
 
@@ -119,19 +64,19 @@ public:
     virtual inline T getData(void) const override final {std::cout << "Get Derived_listener1 data: " << this->a <<std::endl; return this->b;}
     virtual inline void setData(T b_des) override final {b = b_des;};
     virtual inline void display(void) const override final { std::cout << this->a << " " << b << std::endl;}
-    virtual inline void update(std::string message, Status status) const 
+    virtual inline void update(std::string message, Status status, T number) const 
     {
-        std::cout << message << " " << status << std::endl;
+        std::cout << "Listener1:" << message << " " << status << " " << number << std::endl;
     }
 };
 
 template <typename T>
-class Derived_listener1 final : public Base<T>{
+class Derived_listener2 final : public Base<T>{
 protected:
     T b;
 public:
     explicit Derived_listener2(void): // explicit mark constructors to not implicitly convert types
-        Derived_listener1{static_cast<T>(25),static_cast<T>(12)}{std::cout << "Derived_listener2 constructor no args called" << std::endl;};
+        Derived_listener2{static_cast<T>(25),static_cast<T>(12)}{std::cout << "Derived_listener2 constructor no args called" << std::endl;};
     explicit Derived_listener2(T a_, T b_ = 1):
         Base<T>{a_}, b{b_}{
             // a = 4; b = 5;
@@ -139,15 +84,81 @@ public:
         };
     Derived_listener2(const Derived_listener1<T> &d): Base<T>{d}, b{d.b} { std::cout << "Derived_listener2 copy constructor called" << std::endl; }
     // virtual ~Derived_listener1() = default;
-    virtual ~Derived_listener2() {std::cout << "Derived_listener1 destructor called" << std::endl;}
+    virtual ~Derived_listener2() {std::cout << "Derived_listener2 destructor called" << std::endl;}
     virtual inline T getData(void) const override final {std::cout << "Get Derived_listener2 data: " << this->a <<std::endl; return this->b;}
     virtual inline void setData(T b_des) override final {b = b_des;};
     virtual inline void display(void) const override final { std::cout << this->a << " " << b << std::endl;}
-    virtual inline void update(std::string message, Status status) const 
+    virtual inline void update(std::string message, Status status, T number) const 
     {
-        std::cout << message << " " << status << std::endl;
+        std::cout << "Listener2:" << message << " " << status << " " << number << std::endl;
     }
 };
+
+template<typename T>
+class EventManager
+{
+    std::list<Base<T>*> listBase;
+public:
+    EventManager<T>() = default;
+    void notify(std::string message, Status status, T number)
+    {
+        for (auto i : listBase)
+        {
+            i->update(message, status, number);
+        }
+    }
+    void subcribe(Base<T>* base)
+    {
+        listBase.push_back(base);
+    }
+    void unsubcribe(Base<T>* base)
+    {
+        listBase.remove(base);
+    }
+};
+
+template<typename T>
+class EventCreator
+{
+    std::string message;
+    Status status;
+    T number;
+public:
+    EventManager<T> event_manager;
+    EventCreator()
+    {
+        std::cout << "Construct event creator" << std::endl;
+        message = "";
+        status = Status::UNSAVED;
+        number = 0;
+    }
+    void changeMessage(std::string message_)
+    {
+        message = message_;
+        status = Status::CHANGE;
+        number = 101;
+        event_manager.notify(message, status, number);
+    }
+    void saveMessage()
+    {
+        message = "Save";
+        status = Status::SAVED;
+        number = 102;
+        event_manager.notify(message, status, number);
+    }
+    void unsaveMessage()
+    {
+        message = "Unsave";
+        status = Status::UNSAVED;
+        number = 103;
+        event_manager.notify(message, status, number);
+    }
+    ~EventCreator()
+    {
+        std::cout << "Destruct event creator" << std::endl;
+    }
+};
+
 
 template <typename T>
 void use(Base<T>* base) {
@@ -199,6 +210,31 @@ int main(int argc, char const *argv[])
         v->getData();
     }
     */
+
+    // Make event creator
+    PolySample::EventCreator<int> *evenCreator = new PolySample::EventCreator<int>();
+    
+    // Make two listeners
+    PolySample::Base<int> *deriv1 = new PolySample::Derived_listener1<int>();
+    PolySample::Base<int> *deriv2 = new PolySample::Derived_listener2<int>();
+    
+    // event manager add two subcribers
+    evenCreator->event_manager.subcribe(deriv1);
+    evenCreator->event_manager.subcribe(deriv2);
+
+    // event creator change message
+    evenCreator->changeMessage("Hi, I'm event creator");
+
+    // event manager unsubcribe deriv2
+    evenCreator->event_manager.unsubcribe(deriv2);
+
+    // event creator save and unsaved message (some dummy activities that I wrote for fun)
+    evenCreator->saveMessage();
+    evenCreator->unsaveMessage();
+
+    delete deriv1;
+    delete deriv2;
+    delete evenCreator;
 
     // Test explicit
     // PolySample::Base<int> obj;
