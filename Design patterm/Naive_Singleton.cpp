bba20c5e61a278d93367e572607c51d08c4e85a0
QuynhSean <103666326+QuynhSean@users.noreply.github.com>
2024-01-31 13:39:12 +0700

    up load file naive_singleton

---
+++ Design patterm/Naive_Singleton.cpp
@@ -0,0 +1,50 @@
+/**
+ * @file Naive_Singleton.cpp
+ * @author QuynhDT13
+ * @brief 
+ * @version 0.1
+ * @date 2024-01-31
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include <iostream>
+#include <mutex>
+
+using namespace std;
+
+class Base
+{
+private:
+    static mutex mMutex;
+    static Base* instance;
+    int x;
+    Base(/* args */){cout << "Base constructor" << endl;}
+    Base(int x) {cout << "Base parameter constructor" << endl;}   ;
+public:
+    static Base* getintance(){
+        lock_guard<mutex> lock(mMutex);
+        if(instance == nullptr){
+            instance = new Base();
+        }
+        // mLocker.unlock();
+        return instance;
+    }
+    ~Base();
+};
+Base *Base::instance = nullptr;
+mutex Base::mMutex;
+
+Base::~Base()
+{
+}
+int main(){
+    Base *singleton1 = Base::getintance();
+    Base *singleton2 = Base::getintance();
+    cout << &singleton1 << "  " << &singleton2 << endl;
+    /**
+     * @brief Base constructor
+            0xff0f7ffaa8  0xff0f7ffaa0
+     * --> so not is 1 instance :)))
+     */
+}
